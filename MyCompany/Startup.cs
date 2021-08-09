03f8844c5188d1d36a5cbe01bcd17f2f17e2c176
Dmitry-An <AndreykinDm@gmail.com>
2021-08-09 14:16:33 +0400

    +

--- MyCompany/Startup.cs
+++ MyCompany/Startup.cs
@@ -16,6 +16,9 @@ namespace MyCompany
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews().
+                SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
+                .AddSessionStateTempDataProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +30,11 @@ namespace MyCompany
             }
 
             app.UseRouting();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("QQ");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
